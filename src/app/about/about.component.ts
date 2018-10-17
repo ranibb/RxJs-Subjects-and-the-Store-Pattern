87c4287b4d60c9ef912788f69cd16bc6c7a3180f
Rani Abu Sultan <raniabusultan@gmail.com>
2018-10-17 14:23:03 +0300

    A subject is at the same time an observer and an observable.  The idea here is to demonstrate that we have just used the subject to produce a custom observable. The subject is meant to be private to the part of the application that is emitting a given set of data.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
+import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject} from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
@@ -13,14 +13,17 @@ export class AboutComponent implements OnInit {
 
     ngOnInit() {
 
+        const subject = new Subject();
 
+        const series1$ = subject.asObservable();
+
+        series1$.subscribe(console.log)
+
+        subject.next(1);
+        subject.next(2);
+        subject.next(3);
+        subject.complete();
     }
 
 
 }
-
-
-
-
-
-
