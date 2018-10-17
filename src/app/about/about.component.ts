564bcd3599b13dedc59a18c2e2e17a9b608b1f54
Rani Abu Sultan <raniabusultan@gmail.com>
2018-10-17 15:09:57 +0300

    Use AsyncSubject to Receive the last value that got emitted just before completion. The AsyncSubject is ideal for handling long running calculations where we really want only to receive the last value.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject, BehaviorSubject} from 'rxjs';
+import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject, BehaviorSubject, AsyncSubject} from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
@@ -13,7 +13,7 @@ export class AboutComponent implements OnInit {
 
     ngOnInit() {
 
-        const subject = new BehaviorSubject(0);
+        const subject = new AsyncSubject();
 
         const series1$ = subject.asObservable();
 
@@ -27,8 +27,6 @@ export class AboutComponent implements OnInit {
         setTimeout(() => {
 
             series1$.subscribe(val => console.log("late sub:" + val))
-
-            subject.next(4)
 
         }, 3000)
     }
